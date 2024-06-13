c44f96dde95954da4849ba193d138fc07498fe5c
trivikramravi <72453707+trivikramravi@users.noreply.github.com>
2024-06-13 22:00:40 +0530

    added nestlogger in main.ts

--- apps/web-agent/src/main.ts
+++ apps/web-agent/src/main.ts
@@ -5,17 +5,18 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { Logger as NestLogger } from 'nestjs-pino';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(NestLogger));
   const globalPrefix = 'WA';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+     `Application is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
