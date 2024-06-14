65b668c27dedcf08169d0fdf43fa4134ff7a6c91
trivikramravi <72453707+trivikramravi@users.noreply.github.com>
2024-06-14 10:08:45 +0530

    changed the credentials used for mailing

--- apps/web-agent/src/app/utills/mail-service.ts
+++ apps/web-agent/src/app/utills/mail-service.ts
@@ -3,36 +3,38 @@ import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class MailService {
-    private readonly logger: Logger = new Logger(MailService.name)
+  private readonly logger: Logger = new Logger(MailService.name);
   private transporter: nodemailer.Transporter;
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: 'smtp.example.com', // Replace with your SMTP server
-      port: 465, // SSL port
-      secure: true, // Use SSL
+      host: 'email-smtp.us-east-1.amazonaws.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: {
-        user: 'your-email@example.com', // Replace with your email
-        pass: 'your-email-password', // Replace with your email password
+        user: 'AKIA4CHAGTHXCMBLSXJC',
+        pass: 'BHHkWH3HfKLlABcABmkqRHb0N7FDybkCWACZtsndxoJC',
+      },
+      tls: {
+        rejectUnauthorized: false, // Add this if you encounter SSL certificate errors
       },
     });
   }
 
-  async sendMail(to: string, subject: string, text: string, html: string) {
+  async sendMail(subject: string, text: string) {
     const mailOptions = {
-      from: 'your-email@example.com', // Replace with your email
-      to,
+      from:'"Restogpt" <restogpt@orderbyte.io>', //this.configService.get<string>('SENDER_MAIL'),
+      to: "ravitest2@yopmail.com",//this.configService.get<string>('RECIVER_MAIL'),
       subject,
       text,
-      html,
     };
 
     try {
       const info = await this.transporter.sendMail(mailOptions);
-      this.logger.log('Message sent: %s', info.messageId);
+      this.logger.log(`Message sent: ${info.messageId}`);
       return info;
     } catch (error) {
-      this.logger.error('Error sending email: %s', error);
+      this.logger.error(`Error sending email: ${error.message}`);
       throw error;
     }
   }
